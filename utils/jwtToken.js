c14909ff34ed0923af5f0a1228ac8a30484b6b6c
taodxph09094 <taodxph09094@gmail.com>
2024-01-09 14:00:27 +0700

    fix

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -12,7 +12,7 @@ const sendToken = (user, statusCode, res) => {
   };
 
   res.status(statusCode).cookie("token", token, options).json({
-    success: true,
+    status: true,
     user,
     token,
   });
